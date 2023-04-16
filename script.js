f868d26ed45a056bf2feb9aea7b0802b0d5b3537
ayush460 <90319070+ayush460@users.noreply.github.com>
2023-04-16 23:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  let radius = document.getElementById("radius").value;
+  let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(2);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
